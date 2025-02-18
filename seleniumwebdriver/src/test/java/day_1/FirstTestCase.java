a119c21d3e1b522b55952a386ae87e4858d4c6bd
dines <dines@Dinesh>
2025-02-18 22:38:35 +0530

    first code commit

---
+++ seleniumwebdriver/src/test/java/day_1/FirstTestCase.java
@@ -0,0 +1,21 @@
+package day_1;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTestCase {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("https://www.youtube.com/watch?v=gG1Wa6XnIUU");
+		System.out.println("title of page"+driver.getTitle());
+		System.out.println("url of page"+driver.getCurrentUrl());
+		driver.quit();
+		
+	}
+}
