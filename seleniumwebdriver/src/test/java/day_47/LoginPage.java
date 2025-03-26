724467690e5e5d3144897b1e792e64d14e540b63
Dinesh <dineshsai1746@gmail.com>
2025-03-26 17:56:10 +0530

    incomplete...

---
+++ seleniumwebdriver/src/test/java/day_47/LoginPage.java
@@ -0,0 +1,15 @@
+package day_47;
+
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+
+	WebDriver driver;
+	//Constructor
+	LoginPage(WebDriver driver){
+		this.driver=driver;
+	}
+	//Locator
+	
+	//Action method
+}
